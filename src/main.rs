d785d50429c21eec86d0596fdab22518036d886d
Chase Peck <52938508+chasepeck@users.noreply.github.com>
2022-06-25 22:02:04 -0400

    Basic object support

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,53 @@ fn main() {
             Command::new("touch").arg(format!("scripts/{a}/{a}.gml", a=&args[2])).output().expect("Failed to create GML file");
         },
 
-        "object" => todo!(),
+        "object" => {
+            Command::new("mkdir").arg(format!("objects/{}", &args[2])).output().expect("Failed to create object directory");
+            let mut file = std::fs::File::create(format!("objects/{a}/{a}.yy", a=&args[2])).expect("Failed to create YY file");
+            file.write_all(format!("{{\
+              \"spriteId\": null,\
+              \"solid\": false,\
+              \"visible\": true,\
+              \"managed\": true,\
+              \"spriteMaskId\": null,\
+              \"persistent\": true,\
+              \"parentObjectId\": null,\
+              \"physicsObject\": false,\
+              \"physicsSensor\": false,\
+              \"physicsShape\": 1,\
+              \"physicsGroup\": 1,\
+              \"physicsDensity\": 0.5,\
+              \"physicsRestitution\": 0.1,\
+              \"physicsLinearDamping\": 0.1,\
+              \"physicsAngularDamping\": 0.1,\
+              \"physicsFriction\": 0.2,\
+              \"physicsStartAwake\": true,\
+              \"physicsKinematic\": false,\
+              \"physicsShapePoints\": [],\
+              \"eventList\": [\
+                {{\"isDnD\":false,\
+                \"eventNum\":0,\
+                \"eventType\":0,\
+                \"collisionObjectId\":null,\
+                \"resourceVersion\":\"1.0\",\
+                \"name\":\"\",\
+                \"tags\":[],\
+                \"resourceType\":\"GMEvent\",\
+                }},\
+              ],\
+              \"properties\": [],\
+              \"overriddenProperties\": [],\
+              \"parent\": {{\
+                \"name\": \"Objects\",\
+                \"path\": \"folders/Objects.yy\",\
+              }},\
+              \"resourceVersion\": \"1.0\",\
+              \"name\": \"{}\",\
+              \"tags\": [],\
+              \"resourceType\": \"GMObject\",\
+            }}", &args[2]).as_bytes()).expect("Failed to write to YY file");
+            Command::new("touch").arg(format!("objects/{}/Create_0.gml", &args[2])).output().expect("Failed to create GML file");
+        },
 
         _ => panic!("Not a valid/supported asset type: {}", &args[1])
     }
